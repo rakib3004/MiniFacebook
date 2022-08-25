260f1ed8482964b8194a1fe09228691780d09d04
rakib3004 <bsse1129@iit.du.ac.bd>
2022-08-25 11:35:35 +0600

    install everything

--- Database/GomokuTrial/Docker/index.js
+++ Database/GomokuTrial/Docker/index.js
@@ -1,5 +1,6 @@
 
-const app = require('express')();
+const express = require('express');
+var app = express();
 
 app.get('/', (res,req)=>
 res.json({message: 'Institute of Information Technology'})
