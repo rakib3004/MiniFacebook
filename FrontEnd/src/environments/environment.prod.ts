b506b83846af50437313e7d6b85b31f7d41438b8
rakib3004 <bsse1129@iit.du.ac.bd>
2022-09-07 10:24:48 +0600

    import

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://localhost/',
+  storyBaseUrl :"http://127.0.0.1:9000/stories/",
+  profileBaseUrl :"http://127.0.0.1:9000/profiles/",
+  imgExt:".png",
+  fbLogo:"https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg",
+  registrationError:422,
+  serverError:'Opps!! Server not Responding. Please Try Again',
+
+
 };
